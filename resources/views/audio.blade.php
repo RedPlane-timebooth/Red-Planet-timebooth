6995f77f03fdeb8300505bc5ea6d09a756f5dfef
danielpetrov <evilini522912>
2016-03-19 23:04:19 +0200

    title and audio fix

--- resources/views/audio.blade.php
+++ resources/views/audio.blade.php
@@ -1,3 +1,3 @@
-<audio autoplay>
-    <source src="{{'music/glitch/' . str_pad(rand(1,35), 2, '0', STR_PAD_LEFT)  . '.wav'}}" type="audio/ogg">
+<audio  autoplay>
+    <source src="{{'music/glitch/' . str_pad(rand(1,35), 2, '0', STR_PAD_LEFT)  . '.wav'}}" type="audio/mp3">
 </audio>
