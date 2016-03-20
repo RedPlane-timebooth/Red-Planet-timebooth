ea4c0abf97b29aa1812cc9a9050c30db6ad75300
danielpetrov <evilini522912>
2016-03-20 12:22:32 +0200

    audio problem fixed

--- resources/views/audio.blade.php
+++ resources/views/audio.blade.php
@@ -1,3 +1,3 @@
-<audio  autoplay>
-    <source src="{{'music/glitch/' . str_pad(rand(1,35), 2, '0', STR_PAD_LEFT)  . '.wav'}}" type="audio/mp3">
+<audio autoplay>
+    <source src="{{'music/glitch/' . str_pad(rand(1,47), 2, '0', STR_PAD_LEFT)  . '.mp3'}}" type="audio/wav">
 </audio>
