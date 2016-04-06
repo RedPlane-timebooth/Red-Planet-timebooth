44d069e01943dc8bd9f1ebb1edd3b256a5ad05c6
danielpetrov <evilini522912>
2016-04-06 11:53:52 +0300

    login menu in navbar

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -13,8 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
-    <link href="{{ url("/node_modules/bootstrap/dist/css/bootstrap.min.css") }}" rel="stylesheet">
-    <link href="{{ url("/node_modules/bootstrap/dist/css/bootstrap-theme.min.css") }}" rel="stylesheet">
+    <link href="{{ url("https://bootswatch.com/cosmo/bootstrap.min.css") }}" rel="stylesheet">
     <link href="{{ url("/resource/css/stylesheet.css") }}" rel="stylesheet">
     <link href="{{ url("/node_modules/hover.css/css/hover.css") }}" rel="stylesheet" media="all">
 
@@ -39,38 +38,23 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Brand</a>
+                <a class="navbar-brand" href="{{ url('/') }}">Home</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Link</a></li>
+                    <li><a href="{{ url('/shop') }}">Shop</a></li>
+                    <li><a href="{{ url('/about') }}">About</a></li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Game <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">One more separated link</a></li>
+                            <li><a href="{{ url('/game') }}">Play now</a></li>
+                            <li><a href="{{ url('/news') }}">Getting Started</a></li>
+                            {{--or tutorial here--}}
                         </ul>
                     </li>
                 </ul>
-                <form class="navbar-form navbar-left" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
                     @if (Auth::user())
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle hvr-underline-from-center" data-toggle="dropdown"
@@ -87,6 +71,23 @@
                             </ul>
                         </li>
                     @endif
+                    @if (!Auth::user())
+                        <li>
+                            <form class="navbar-form" name="loginForm" method="POST" action="{{ url('/login') }}">
+                                <div class="form-group" style="height: 30px; vertical-align: baseline;" >
+                                    <input type="text" required placeholder="E-mail" class="form-control"/>
+                                    <input type="password" required placeholder="Password" class="form-control"/>
+                                </div>
+                                <button type="submit">Login</button>
+                            </form>
+                        </li>
+                            <li><a href="{{ url('/register')}}">Register</a></li>
+                        @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 
 </header>
     @yield('content')
